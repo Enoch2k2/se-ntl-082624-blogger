183b531ef004d48e05b9d2f06ba1050ea3afc59d
Enoch2k2 <enoch2k2@hotmail.com>
2024-09-18 10:58:06 -0500

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,6 @@ import Header from './components/Header'
 import { blogs } from './data'
 
 function App() {
-  
-  // console.log(blogs)
-  // function displayH1() {
-  //   if(false) {
-  //     return <h1>Hello World</h1>
-  //   } else {
-  //     return <h1>Goodbye World!</h1>
-  //   }
-  // }
 
   return (
     <>
@@ -20,20 +11,5 @@ function App() {
     </>
   )
 }
-
-// const App = () => {
-//   return (
-//     <>
-//       <h1>Hello World</h1>
-//     </>
-//   )
-// }
-
-
-// {true ? <div>
-//   <h1>Hello World</h1>
-//   <h3>{name}</h3>
-//   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ab impedit blanditiis libero explicabo, perferendis provident quis iusto ex dolorem enim iure, dolore quod eveniet omnis rem? Corporis, maiores nostrum?</p>
-// </div> : <h1>Goodbye World</h1>}
 
 export default App
